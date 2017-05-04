53f0e5bb550b3ab039da214af76ea2c039dfaa85
atbj505 <atbj505@hotmail.com>
2017-05-04 21:56:49 +0800

    comics url

--- Comics/Comics/spiders/comics.py
+++ Comics/Comics/spiders/comics.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 
 import scrapy
+from bs4 import BeautifulSoup
 
 
 class Comics(scrapy.Spider):
@@ -16,4 +17,14 @@ class Comics(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        self.log(response.body)
+        content = response.body
+        soup = BeautifulSoup(content, "html5lib")
+        listcon_tag = soup.find('ul', class_='listcon')
+        com_a_list = listcon_tag.find_all('a', attrs={'href': True})
+
+        comics_url_list = []
+        base = 'http://www.xeall.com'
+        for tag_a in com_a_list:
+            url = base + tag_a['href']
+            self.log(url)
+            comics_url_list.append(url)
