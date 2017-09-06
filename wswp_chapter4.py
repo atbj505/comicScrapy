9d54ae2329c00e4d9e5c1f063d5f28a9a95ebd38
atbj505 <atbj505@hotmail.com>
2017-09-06 15:37:43 +0800

    format

--- wswp_chapter4.py
+++ wswp_chapter4.py
@@ -16,16 +16,20 @@ def main():
     links = driver.find_elements_by_css_selector('#results a')
     countries = [link.text for link in links]
     print(countries)
+    driver.close()
+
 
 def vpn_test():
     # driver = webdriver.PhantomJS(executable_path = './phantomjs')
     driver = webdriver.PhantomJS()
-    driver.get('http://www.site-digger.com/html/articles/20110516/proxieslist.html')
+    driver.get(
+        'http://www.site-digger.com/html/articles/20110516/proxieslist.html')
     trs = driver.find_elements_by_tag_name('tr')
 
     for tr in trs:
         # print(tr.text)
         print(tr.text.split(' ')[0])
+
 
 if __name__ == "__main__":
     # main()
